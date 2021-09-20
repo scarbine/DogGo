fbbc1a9cdae00584c2e2dd5df04024a68bea2e6d
Sam Carbine <scarbine@me.com>
2021-09-20 11:53:17 -0500

    added view to list all of the walkers

---
+++ DogGo/Repositories/IWalkerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using DogGo.Models;
+
+namespace DogGo.Repositories
+{
+        public interface IWalkerRepository
+        {
+            List<Walker> GetAllWalkers();
+            Walker GetWalkerById(int id);
+        }
+
+   
+}

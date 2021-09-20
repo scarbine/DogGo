11f4acee42b63932e19865c016306e3410ac19ea
Sam Carbine <scarbine@me.com>
2021-09-20 10:40:55 -0500

    added Walker.cs and Neighborhood.cs

---
+++ DogGo/Models/Neighborhood.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Models
+{
+    public class Neighborhood
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}

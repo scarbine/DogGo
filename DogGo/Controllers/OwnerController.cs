d9fd0c14a193e66e338bd24c09e161fb39bc804f
Sam Carbine <scarbine@me.com>
2021-09-21 12:02:52 -0500

    added an affordance for the user to edit a reccord

--- DogGo/Controllers/OwnerController.cs
+++ DogGo/Controllers/OwnerController.cs
@@ -64,21 +64,28 @@ namespace DogGo.Controllers
         // GET: OwnerController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            Owner owner = _ownerRepo.GetOwnerById(id);
+
+            if (owner ==null)
+            {
+                return NotFound();
+            }
+            return View(owner);
         }
 
         // POST: OwnerController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Owner owner)
         {
             try
             {
+                _ownerRepo.UpdateOwner(owner);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch(Exception ex)
             {
-                return View();
+                return View(owner);
             }
         }
 

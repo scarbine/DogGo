9b1af3b1d7f4c25b595570998de6290a1a10cd1e
Sam Carbine <scarbine@me.com>
2021-09-22 09:58:18 -0500

    adding a drop down affrodance for the user to select a neighborhood when the user is created

--- DogGo/Controllers/OwnerController.cs
+++ DogGo/Controllers/OwnerController.cs
@@ -15,17 +15,21 @@ namespace DogGo.Controllers
         private readonly IOwnerRepository _ownerRepo;
         private readonly IDogRepository _dogRepo;
         private readonly IWalkerRepository _walkerRepo;
+        private readonly INeighborhoodRepository _neighborhoodRepo;
 
 
         // ASP.NET will give us an instance of our Walker Repository. This is called "Dependency Injection"
         public OwnerController(IOwnerRepository ownerRepository,
             IDogRepository dogRepository,
-            IWalkerRepository walkerRepository)
+            IWalkerRepository walkerRepository,
+            INeighborhoodRepository neighborhoodRepository)
         {
             
             _ownerRepo = ownerRepository;
             _dogRepo = dogRepository;
             _walkerRepo = walkerRepository;
+            _neighborhoodRepo = neighborhoodRepository;
+            
         }
 
 
@@ -56,7 +60,15 @@ namespace DogGo.Controllers
         // GET: OwnerController/Create
         public ActionResult Create()
         {
-            return View();
+            List<Neighborhood> neighborhoods = _neighborhoodRepo.GetAll();
+
+            OwnerFormViewModel vm = new OwnerFormViewModel()
+            {
+                Owner = new Owner(),
+                Neighborhoods = neighborhoods
+            };
+
+            return View(vm);
         }
 
         // POST: OwnerController/Create
